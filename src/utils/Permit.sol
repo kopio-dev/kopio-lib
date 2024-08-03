e14dbedf35311a6934e98108b4de75ffaf78df8d
fxp3 <dev@0xp.lol>
2024-08-03 06:24:40 +0300

    chore: add token permit

--- src/utils/Permit.sol
+++ src/utils/Permit.sol
@@ -1,12 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import {IERC20Permit} from "../token/IERC20Permit.sol";
 
 library Permit {
-    bytes32 internal constant _PERMIT_TYPEHASH =
+    bytes32 internal constant PERMIT_TYPEHASH =
         keccak256(
             "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
         );
-    function get(
+
+    function getPermitHash(
+        IERC20Permit token,
+        address owner,
+        address spender,
+        uint256 amount,
+        uint256 deadline
+    ) public view returns (bytes32) {
+        return
+            getPermitHash(
+                owner,
+                spender,
+                amount,
+                token.nonces(owner),
+                deadline,
+                token.DOMAIN_SEPARATOR()
+            );
+    }
+
+    function getPermitHash(
         address owner,
         address spender,
         uint256 value,
@@ -21,7 +41,7 @@ library Permit {
                     domainSeparator,
                     keccak256(
                         abi.encode(
-                            _PERMIT_TYPEHASH,
+                            PERMIT_TYPEHASH,
                             owner,
                             spender,
                             value,
