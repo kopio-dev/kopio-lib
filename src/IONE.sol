04567599967be0a2ae67ecba1b7fdc44e5eae91f
fxp3 <dev@0xp.lol>
2024-08-19 14:59:29 +0300

    chore: update ione and ivault

--- src/IONE.sol
+++ src/IONE.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.0;
 
 import {IKopioIssuer, IVaultExtender} from "./IKopioCore.sol";
-import {IVault} from "./IVault.sol";
+import {IVault, IVaultFlashReceiver} from "./IVault.sol";
 import {IERC20Permit} from "./token/IERC20Permit.sol";
 import {IERC165} from "./vendor/IERC165.sol";
 
-interface IONE is IERC20Permit, IVaultExtender, IKopioIssuer, IERC165 {
+interface IONE is
+    IERC20Permit,
+    IVaultExtender,
+    IVaultFlashReceiver,
+    IKopioIssuer,
+    IERC165
+{
     function protocol() external view returns (address);
     function issue(uint256 amount, address to) external returns (uint256);
     function destroy(uint256 amount, address from) external returns (uint256);
--- src/IVault.sol
+++ src/IVault.sol
@@ -3,11 +3,35 @@ pragma solidity ^0.8.0;
 import {IERC20Permit, IERC20} from "./token/IERC20Permit.sol";
 import {IAggregatorV3} from "./vendor/IAggregatorV3.sol";
 
-interface VEvent {
-    /* -------------------------------------------------------------------------- */
-    /*                                   Events                                   */
-    /* -------------------------------------------------------------------------- */
+interface IVaultFlash {
+    enum FlashKind {
+        Shares,
+        Assets
+    }
 
+    struct Flash {
+        address asset;
+        uint256 assets;
+        uint256 shares;
+        address receiver;
+        FlashKind kind;
+    }
+
+    struct FlashData {
+        uint256 balIn;
+        uint256 tSupplyIn;
+        uint256 tAssetsIn;
+        uint256 depositsIn;
+    }
+
+    error FLASH_KIND_NOT_SUPPORTED(FlashKind);
+}
+
+interface IVaultFlashReceiver is IVaultFlash {
+    function onVaultFlash(Flash calldata, bytes calldata) external;
+}
+
+interface VEvent {
     /**
      * @notice Emitted when a deposit/mint is made
      * @param caller Caller of the deposit/mint
@@ -44,20 +68,14 @@ interface VEvent {
      * @notice Emitted when a new asset is added to the shares contract
      * @param asset Address of the asset
      * @param feed Price feed of the asset
-     * @param symbol Asset symbol
      * @param staletime Time in seconds for the feed to be considered stale
-     * @param price Price of the asset
      * @param depositLimit Deposit limit of the asset
-     * @param timestamp Timestamp of the addition
      */
     event AssetAdded(
         address indexed asset,
         address indexed feed,
-        string indexed symbol,
         uint256 staletime,
-        uint256 price,
-        uint256 depositLimit,
-        uint256 timestamp
+        uint256 depositLimit
     );
 
     /**
@@ -95,6 +113,15 @@ interface VEvent {
         uint256 assetsOut,
         uint256 sharesIn
     );
+    event VaultFlash(
+        address indexed caller,
+        address indexed owner,
+        address indexed asset,
+        address receiver,
+        uint256 assets,
+        uint256 shares,
+        IVaultFlash.FlashKind kind
+    );
 }
 
 struct VaultAsset {
@@ -117,7 +144,7 @@ struct VaultConfiguration {
     uint8 oracleDecimals;
 }
 
-interface IVault is IERC20Permit, VEvent {
+interface IVault is IERC20Permit, IVaultFlash, VEvent {
     function deposit(
         address assetAddr,
         uint256 assetsIn,
@@ -144,6 +171,23 @@ interface IVault is IERC20Permit, VEvent {
         address owner
     ) external returns (uint256 sharesIn, uint256 assetFee);
 
+    function flash(
+        address assetAddr,
+        uint256 assetsOut,
+        address receiver,
+        address owner,
+        bytes calldata args
+    ) external returns (uint256 sharesIn, uint256 assetFee);
+
+    function flash(
+        address assetAddr,
+        uint256 sharesOut,
+        address receiver,
+        bytes calldata args
+    ) external returns (uint256 assetsIn, uint256 assetFee);
+
+    function getFees(address assetAddr) external view returns (uint256);
+
     function getConfig()
         external
         view
@@ -153,80 +197,58 @@ interface IVault is IERC20Permit, VEvent {
 
     function allAssets() external view returns (VaultAsset[] memory assets);
 
-    function assetList(uint256 index) external view returns (address assetAddr);
+    function assetList(uint256) external view returns (address assetAddr);
 
     function assets(address) external view returns (VaultAsset memory asset);
 
     function assetPrice(address assetAddr) external view returns (uint256);
-
     function previewDeposit(
         address assetAddr,
         uint256 assetsIn
     ) external view returns (uint256 sharesOut, uint256 assetFee);
-
     function previewMint(
         address assetAddr,
         uint256 sharesOut
     ) external view returns (uint256 assetsIn, uint256 assetFee);
-
     function previewRedeem(
         address assetAddr,
         uint256 sharesIn
     ) external view returns (uint256 assetsOut, uint256 assetFee);
-
     function previewWithdraw(
         address assetAddr,
         uint256 assetsOut
     ) external view returns (uint256 sharesIn, uint256 assetFee);
-
     function maxDeposit(
         address assetAddr
     ) external view returns (uint256 assetsIn);
-
+    function maxDeposit(address, address) external view returns (uint256);
     function maxMint(
-        address assetAddr,
-        address owner
+        address assetAddr
     ) external view returns (uint256 sharesOut);
-
+    function maxMint(address, address) external view returns (uint256);
     function maxRedeem(
-        address assetAddr,
-        address owner
+        address assetAddr
     ) external view returns (uint256 sharesIn);
-
+    function maxRedeem(address, address) external view returns (uint256);
     function maxWithdraw(
-        address assetAddr,
-        address owner
+        address assetAddr
     ) external view returns (uint256 amountOut);
-
+    function maxWithdraw(address, address) external view returns (uint256);
     function exchangeRate() external view returns (uint256 rate);
-
     function setBaseRate(uint256 newBaseRate) external;
-
-    function addAsset(
-        VaultAsset memory assetConfig
-    ) external returns (VaultAsset memory);
-
+    function addAsset(VaultAsset memory) external returns (VaultAsset memory);
     function removeAsset(address assetAddr) external;
-
     function setGovernance(address newGovernance) external;
-
     function acceptGovernance() external;
-
     function setFeeRecipient(address newFeeRecipient) external;
-
     function setAssetFeed(
         address assetAddr,
         address feedAddr,
         uint24 newStaleTime
     ) external;
-
     function setFeedPricePrecision(uint8 newDecimals) external;
-
     function setMaxDeposits(address assetAddr, uint248 newMaxDeposits) external;
-
     function setAssetEnabled(address assetAddr, bool isEnabled) external;
-
     function setDepositFee(address assetAddr, uint16 newDepositFee) external;
-
     function setWithdrawFee(address assetAddr, uint16 newWithdrawFee) external;
 }
